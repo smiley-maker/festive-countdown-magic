e1715a5b14f719a5b37199a1c4d805a462a5d131
jorsincl <jordan.sinclair@du.edu>
2024-12-31 20:52:13 -0700

    It works

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,9 +7,6 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-[#1A1F2C] flex flex-col items-center justify-start p-4">
       <Particles />
-      <h1 className="text-4xl md:text-6xl font-bold text-white mb-12 text-center relative z-10">
-        New Year Countdown
-      </h1>
       <div className="relative z-10 w-full">
         <CountdownTimer />
         <ResolutionWall />
